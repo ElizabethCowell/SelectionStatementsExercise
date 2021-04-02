9e5edecb253423ff24a61ecc11bb0093fd6da4a8
ElizabethCowell <80732868+ElizabethCowell@users.noreply.github.com>
2021-04-02 15:51:27 -0400

    assignment complete

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace SelectionStatementsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+
+            Console.WriteLine("Try to guess my favorite number!");
+            var userInput = int.Parse(Console.ReadLine());
+            if (userInput > favNumber)
+            {
+                Console.WriteLine("too high");
+            }
+            else if (userInput < favNumber)
+            {
+                Console.WriteLine("too low");
+            }
+            else
+            {
+                Console.WriteLine("Nevermind");
+            }
+
+            Console.WriteLine("What is your favorite subject");
+            string subject = Console.ReadLine();
+
+            switch (subject)
+            {
+                case "Math":
+                    Console.WriteLine("Math");
+                    break;
+                case "Art":
+                    Console.WriteLine("Art");
+                    break;
+                case "History":
+                    Console.WriteLine("History");
+                    break;
+                case "Science":
+                    Console.WriteLine("Science");
+                    break;
+                default:
+                    Console.WriteLine("New Subject");
+                    break;
+
+            }
+
+        }
+    }
+}
